5972a6bebda4282414d669971e1eeeded7ad779b
Andre Fernandes <andre@vertigo.com.br>
2017-09-29 09:45:13 -0300

    mvn-docker

---
+++ mvn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Linha de comando "mvn" para quem não tem Java instalado.
+# Quem tem Docker não tem medo.
+# Usar imagem "maven" ou "vertigo/maven-ibmjava"
+#
+docker run --rm -ti \
+  -p 9080:9080 \
+  -v $(pwd):/opt/app \
+  -w /opt/app \
+  -e TERM=xterm \
+  -v $HOME/.m2:/root/.m2 \
+  vertigo/maven-ibmjava mvn "$@"
